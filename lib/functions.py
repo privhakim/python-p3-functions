416beb0b29a3f096fc66d358f5c9e333d4fd7acf
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-08-16 21:26:34 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    return 'Hello, programmer!\n'
 
 def greet(name):
-    pass
+    return f'Hello, {name}!'
 
-def greet_with_default(name="programmer"):
-    pass
+def greet_with_default(name='programmer!\n'):
+    return f"Hello, {name}!"
 
 def add(num1, num2):
-    pass
-
+    return num1 + num2
+               
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
